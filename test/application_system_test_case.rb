f51652870f388551c6af3d071574097d7ddee851
Aziza Gentles <azizagentles@gmail.com>
2026-05-10 16:15:32 -0700

    System fix two

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 require "selenium/webdriver"
 
-class ApplicationSystemTestCase
+class ApplicationSystemTestCase <
   ActionDispatch::SystemTestCase
   driven_by :selenium,
   using: :headless_chrome, screen_size: [ 1400, 1400 ]
